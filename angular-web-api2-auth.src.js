3b509f9aa148fb8c078231c8cb1a8791e3ecd14f
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-04-09 23:33:48 +0200

    formatting

--- angular-web-api2-auth.src.js
+++ angular-web-api2-auth.src.js
@@ -1,84 +1,79 @@
 angular.module('kennethlynne.webAPI2Authentication', [])
-    .provider('webAPIAuth', function ($httpProvider) {
+  .provider('webAPIAuth', function ($httpProvider) {
 
-        var tokenUrl = '';
+    var tokenUrl = '',
+      endpointUrl = '';
 
-        var endpointUrl = '';
+    $httpProvider.interceptors.push(['$q', '$injector', function ($q, $injector) {
+      return {
+        request: function (cfg) {
+          var token = $injector.get('webAPIAuth').getToken();
+          var matchesAPIUrl = cfg.url.substr(0, endpointUrl.length) === endpointUrl;
 
-        $httpProvider.interceptors.push(['$q', '$injector', function ($q, $injector) {
-            return {
-                request: function (cfg) {
-                    var token = $injector.get('webAPIAuth').getToken();
-                    var matchesAPIUrl = cfg.url.substr(0, endpointUrl.length) === endpointUrl;
+          if (token && matchesAPIUrl) {
+            cfg.headers['Authorization'] = 'Bearer ' + token;
+          }
+          return cfg || $q.when(cfg);
+        }
+      };
+    }]);
 
-                    if (token && matchesAPIUrl) {
-                        cfg.headers['Authorization'] = 'Bearer ' + token;
-                    }
-                    return cfg || $q.when(cfg);
-                }
+    this.setTokenEndpointUrl = function (url) {
+      tokenUrl = url;
+    };
+
+    this.setAPIUrl = function (url) {
+      endpointUrl = url;
+    };
+
+    this.$get = ['$http', '$window', '$log', '$q', function ($http, $window, $log, $q) {
+
+      var localStorage = $window.localStorage,
+        _logout = function () {
+          localStorage.removeItem('token');
+        },
+        _getToken = function () {
+          return localStorage.getItem('token');
+        },
+        _login = function (grantType, username, password) {
+          var deferred = $q.defer(),
+            cfg = {
+              method: 'POST',
+              url: tokenUrl,
+              data: 'grant_type=' + grantType + '&username=' + username + '&password=' + password,
+              headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             };
-        }]);
 
-        this.setTokenEndpointUrl = function (url) {
-            tokenUrl = url;
-        };
-
-        this.setAPIUrl = function (url) {
-            endpointUrl = url;
-        };
-
-        this.$get = ['$http', '$window', '$log', '$q', function ($http, $window, $log, $q) {
-
-            var localStorage = $window.localStorage;
-
-            var _logout = function () {
-                    localStorage.removeItem('token');
-                },
-                _getToken = function () {
-                    return localStorage.getItem('token');
-                },
-                _login = function (grantType, username, password) {
-
-                    var deferred = $q.defer();
-
-                    var cfg = {
-                        method: 'POST',
-                        url: tokenUrl,
-                        data: 'grant_type=' + grantType + '&username=' + username + '&password=' + password,
-                        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                    };
-
-                    $http(cfg).then(function (response) {
-                        if (response && response.data) {
-                            var data = response.data;
-                            localStorage.setItem('token', data.access_token);
-                            deferred.resolve(true);
-                        }
-                        else
-                        {
-                            deferred.reject('No data received');
-                        }
-                    })
-                        .catch(function (response) {
-                            var message = (response && response.data && response.data.message) ? response.data.message : '';
-                            deferred.reject('Could not log you in. ' + message);
-                        })
-                        .finally(function () {
-                            $log.log('Log in request finished.');
-                        });
-
-                    return deferred.promise;
-
-                },
-                _isLoggedIn = function () {
-                    return typeof _getToken() == 'string';
-                };
-
-            return {
-                isLoggedIn: _isLoggedIn,
-                login: _login,
-                getToken: _getToken,
-                logout: _logout
+          $http(cfg).then(function (response) {
+            if (response && response.data) {
+              var data = response.data;
+              localStorage.setItem('token', data.access_token);
+              deferred.resolve(true);
             }
-        }];
-    });
+            else {
+              deferred.reject('No data received');
+            }
+          })
+            .catch(function (response) {
+              var message = (response && response.data && response.data.message) ? response.data.message : '';
+              deferred.reject('Could not log you in. ' + message);
+            })
+            .finally(function () {
+              $log.log('Log in request finished.');
+            });
+
+          return deferred.promise;
+
+        },
+        _isLoggedIn = function () {
+          return typeof _getToken() == 'string';
+        };
+
+      return {
+        isLoggedIn: _isLoggedIn,
+        login: _login,
+        getToken: _getToken,
+        logout: _logout
+      }
+    }];
+  });
